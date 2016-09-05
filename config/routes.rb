f0cf72e6b9aefca95443e19a2ad51be05caeb0b8
z1ho <ziho.89@gmail.com>
2016-09-05 22:23:27 +0800

    Changes to Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,75 @@
+# Rails.application.routes.draw do
+
+
+#   resources :listings
+
+#   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+#   resource :session, controller: "clearance/sessions", only: [:create]
+
+#   resources :users, controller: "clearance/users", only: [:create] do
+#     resource :password,
+#       controller: "clearance/passwords",
+#       only: [:create, :edit, :update]
+#   end
+
+#   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+#   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+#   get "/sign_up" => "clearance/users#new", as: "sign_up"
+#   get 'welcome/index'
+#   resources :users, only: [:show, :edit, :update, :destroy]
+
+#   root 'welcome#index'
+#   get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+
+
 Rails.application.routes.draw do
+
+  # Landing Page
+  root 'sessions#new'
+
+  # Post SIGN-IN redirect to Homepage
+  get 'static/home' => 'static#home'
+
+  # USERS
+  resources :users, only: [:show, :edit, :update, :destroy]
+
+  # PROFILE
+  get '/profile', to: "users#profile", as: "my_profile"
+
+  # FACEBOOK SIGN-IN
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth", as: "callback"
+
+  # SESSIONS
+  # get 'sessions/show'
+
+  # LISTINGS
+  resources :listings do
+    resources :reservations
+  end
+
+  resources :reservations, only: [] do
+      resources :payments, only: [:new, :create]
+  end
+
+  # CATEGORIES
+  get 'tags/:tag', to: 'listings#index', as: :tag
+
+ get 'search', to: 'search#search'
+ post 'search', to: 'search#search'
+
+ get 'listings/template'
+
+  # PAYMENTS
+  # get 'payments/new/:id', to: "payments#new"
+
+  # Makes SIGN-UP the landing page
+  # root 'clearance/users#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'sessions#new'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
