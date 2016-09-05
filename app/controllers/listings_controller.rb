2daeee3be41b373218d7d90a739737cd5b2efd69
z1ho <ziho.89@gmail.com>
2016-09-05 22:29:42 +0800

    Changes to Models

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,54 @@
 class ListingsController < ApplicationController
+  def index   
+    @user = current_user
+    if params[:tag]
+      @listings = Listing.tagged_with(params[:tag])
+      # @listings.order(updated_at: :desc)
+    else
+      @listings = Listing.all
+    end
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+    @user == current_user
+    @reserve = @listing.reservations.new
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def create
+    @listing = current_user.listings.new(listing_params)
+    @listing.save
+    redirect_to listings_path
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+    @listing.update(listing_params)
+      if @listing.save
+        redirect_to listings_path
+        flash[:success] = "Succesfully updated!"
+      else
+        flash[:warning] = "Error, no modifications made. Please try again."  
+        render :edit
+      end      
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    redirect_to listings_path
+  end
+
+  private
+  def listing_params
+    params.require(:listing).permit(:name, :description, :country, :city, :address, :room_type, :property_type, :accomodates, :num_of_beds, :num_of_bathrooms, :price, :tag_list, {image: []})
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < ApplicationController
+
+  def show
+  	@user = User.find(params[:id])  
+  end
+
+  def profile
+  	@user = current_user
+  end
+
+  def edit
+  	@user = User.find(params[:id])  
+  end
+
+
+  def update
+	@user = User.find(params[:id]) 
+	# @user.update_attributes(user_params_update)
+	@user.update(user_params_update)
+
+	if @user.save
+		redirect_to my_profile_path
+		flash[:success] = "Successfully updated details!"
+		else
+			# flash[:danger] = "Error, details not updated"
+			render :edit
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :email, :gender, :birthday, :subscribe, :encrypted_password, :avatar, :about, :reservations)
+	end
+
+	def user_params_update
+		params.require(:user).permit(:first_name, :last_name, :email, :gender, :birthday, :subscribe, :about).merge(encrypted_password: @user.encrypted_password)
+	end
+
+	def check_user
+		unless current_user == User.find(params[:id])
+			flash[:danger] = "Error, please try again"
+			redirect_to '/'
+		end
+	end
+	
 end
